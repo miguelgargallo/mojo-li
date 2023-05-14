964f5854557de9aec898cdeaf64c5361df11f1c6
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 19:04:38 +0200

    gh 0.0.0 Source Code

--- app/page.tsx
+++ app/page.tsx
@@ -45,13 +45,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Official 🔥{' '}
+            Read more 🔥{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover Mojo in english and get started with the official guide.
+            Discover the official site, we are a side project not official and we are not affiliated with Modular.
           </p>
         </a>
         <a
