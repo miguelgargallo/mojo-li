9844ade0e0c9ff57bd5b5078ed8958f0d9565fa9
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:03:33 +0200

    gh 0.0.0 Source Code

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          This site is under construction&nbsp;
-          <code className="font-mono font-bold">See you soon</code>
+          Mojo Li 🔥&nbsp;
+          <code className="font-mono font-bold">Educative Programming language for all of AI site</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <p className="text-center text-xs text-gray-500 dark:text-gray-400">
-            Powered by&nbsp;
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://www.modular.com/mojo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
             <a
               className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
               href="https://www.modular.com/mojo"
@@ -28,7 +33,7 @@ export default function Home() {
             >
               Pylar AI
             </a>
-          </p>
+          </a>
         </div>
       </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
@@ -51,12 +56,12 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover the official site, we are a side project not official and we are not affiliated with Modular.
+            Discover Mojo here.
           </p>
         </a>
         <a
           href="https://pylar.org"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -101,6 +106,16 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <footer className="flex flex-col items-center justify-center w-full h-24 border-t border-gray-200 dark:border-neutral-700">
+        <div className="flex items-center justify-center">
+          <a
+            className="flex items-center justify-center"
+            href="https://mojo.li" target="_blank" rel="noopener noreferrer"
+          >
+            Mojo 🔥 Li. 2023 All rights reserved. mojo.li is not an official site or affiliated with Modular.
+          </a>
+        </div>
+      </footer>
     </main>
   )
 }
