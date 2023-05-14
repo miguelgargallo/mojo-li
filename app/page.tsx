9ce9ac8b9cbf52836250ba930c345add08b7d58a
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:10:00 +0200

    gh 0.0.1 fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,33 +6,17 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Mojo Li 🔥&nbsp;
-          <code className="font-mono font-bold">Educative Programming language for all of AI site</code>
+          <code className="font-mono font-bold">Mojo Programming language educative site</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          Read the code on&nbsp;
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.modular.com/mojo"
+            className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
+            href="https://github.com/miguelgargallo/mojo-li"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <a
-              className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
-              href="https://www.modular.com/mojo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              MOJO 🔥
-            </a>
-            &nbsp;+&nbsp;
-            <a
-              className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
-              href="https://pylar.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Pylar AI
-            </a>
+            GitHub
           </a>
         </div>
       </div>
