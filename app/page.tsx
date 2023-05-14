8b91d5bd4de6486f38eed7fe6513a6c486b20dfa
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:23:37 +0200

    gh 0.0.1 fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Mojo Li 🔥&nbsp;
-          <code className="font-mono font-bold">Mojo Programming language educative site</code>
+          <code className="font-mono font-bold hidden lg:inline">Mojo Programming language educative site</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           Read the code on&nbsp;
@@ -20,13 +18,13 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="py-8 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <h1 className="relative z-10 text-6xl font-bold text-center text-gray-900 dark:text-gray-100 lg:text-8xl">
-          <span className="block">Welcome to</span>
-          <span className="block">Mojo.li 🔥 + Pylar AI</span>
+          <span className="block">Under</span>
+          <span className="block">construction</span>
         </h1>
       </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="py-8 mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://www.modular.com/mojo"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -40,7 +38,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover Mojo here.
+            Discover the official Mojo site by Modular.
           </p>
         </a>
         <a
@@ -86,20 +84,9 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover Miguel Gargallo, AI divulgator and entrepreneur.
-          </p>
+            Mojo 🔥 Li. 2023 All rights reserved. mojo.li is not an official site or affiliated with Modular.          </p>
         </a>
       </div>
-      <footer className="flex flex-col items-center justify-center w-full h-24 border-t border-gray-200 dark:border-neutral-700">
-        <div className="flex items-center justify-center">
-          <a
-            className="flex items-center justify-center"
-            href="https://mojo.li" target="_blank" rel="noopener noreferrer"
-          >
-            Mojo 🔥 Li. 2023 All rights reserved. mojo.li is not an official site or affiliated with Modular.
-          </a>
-        </div>
-      </footer>
     </main>
   )
 }
