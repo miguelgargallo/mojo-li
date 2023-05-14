68ff331b5c28c215d45961e83dcece5edf3da5c7
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:48:23 +0200

    gh 0.0.1 fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Mojo LI 🔥&nbsp;
+          Mojo ES 🔥&nbsp;
           <code className="font-mono font-bold hidden lg:inline">Aprende el nuevo lenguaje de programación Mojo en Castellano.</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          Somos codigo abierto en&nbsp;
+          Somos código abierto en&nbsp;
           <a
             className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
             href="https://github.com/miguelgargallo/mojo-cat"
@@ -62,7 +62,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Mojo.li 🔥 Cat {' '}
+            Mojo Cat 🔥{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
