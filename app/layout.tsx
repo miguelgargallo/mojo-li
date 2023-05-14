cbe0f88c7501f12aefd06f89eae83433a15fe258
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:56:30 +0200

    gh 0.0.1 fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Mojo ES - Mojo en castellano',
-  description: 'Primeros en el mundo en educar el lenguje de programación Mojo en castellano',
+  title: 'Mojo ES - Primeros en el mundo en educar IA',
+  description: 'Integración de Inteligència Artificial en la educación',
 }
 
 export default function RootLayout({
