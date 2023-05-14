5078bd5589f6eb35fe0b3c5e21901b036b5df295
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 20:42:40 +0200

    gh 0.0.1 fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Mojo Li 🔥&nbsp;
-          <code className="font-mono font-bold hidden lg:inline">Mojo Programming language educative site</code>
+          Mojo LI 🔥&nbsp;
+          <code className="font-mono font-bold hidden lg:inline">Aprende el nuevo lenguaje de programación Mojo en Castellano.</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          Read the code on&nbsp;
+          Somos codigo abierto en&nbsp;
           <a
             className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
-            href="https://github.com/miguelgargallo/mojo-li"
+            href="https://github.com/miguelgargallo/mojo-cat"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -20,8 +20,8 @@ export default function Home() {
       </div>
       <div className="py-8 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <h1 className="relative z-10 text-6xl font-bold text-center text-gray-900 dark:text-gray-100 lg:text-8xl">
-          <span className="block">Under</span>
-          <span className="block">construction</span>
+          <span className="block">Pronto</span>
+          <span className="block">Online</span>
         </h1>
       </div>
       <div className="py-8 mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -32,13 +32,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Read more 🔥{' '}
+            Lee más 🔥{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover the official Mojo site by Modular.
+            Descubre el sitio oficial de Modular.
           </p>
         </a>
         <a
@@ -54,21 +54,21 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover Pylar AI, the power of AI for your business.
+            Integramos Pylar AI en tu negocio verticalmente.
           </p>
         </a>
         <a
-          href="https://mojo.cat"
+          href="https://mojo.li"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Mojo.cat 🔥 {' '}
+            Mojo.li 🔥 Cat {' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            We are Mojo CAT too!
+            Mojo en catalán.
           </p>
         </a>
         <a
@@ -84,7 +84,8 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Mojo 🔥 Li. 2023 All rights reserved. mojo.li is not an official site or affiliated with Modular.          </p>
+            Mojo LI 🔥. 2023 Todos los derechos reservados. mojo.cat no es un sitio oficial ni afiliado a Modular.
+          </p>
         </a>
       </div>
     </main>
